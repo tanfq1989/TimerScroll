b878b9bffaf97cd16a42fc6a2e570b561eadbb91
tanfq1989 <tanfq1989@hotmail.com>
2015-03-23 15:58:11 +0800

    no message

--- Example/Pods/Target Support Files/Pods-TimerScroll/Pods-TimerScroll-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TimerScroll : NSObject
-@end
-@implementation PodsDummy_Pods_TimerScroll
-@end
--- Example/Pods/Target Support Files/Pods-TimerScroll/Pods-TimerScroll-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_TimerScrollVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_TimerScrollVersionString[];
-
